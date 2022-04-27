b1b62bfa8552072324f07dc40e27c41e8ab32fc1
Chase Lipari <75291063+Chasedude44@users.noreply.github.com>
2022-04-27 13:53:34 -0400

    Finished Errors

--- models/errorModel.js
+++ models/errorModel.js
@@ -1,14 +1,43 @@
 class InvalidInputError extends Error {
-    
+    string _module;
+    string _method;
+    string _message
+    public InvalidInputError(string module, string method, string message){
+        this._module = module;
+        this._method = method;
+        this._message = message;
+    }
 };
 class DatabaseError extends Error {
-
+    string _module;
+    string _method;
+    string _message
+    public DatabaseError(string module, string method, string message){
+        this._module = module;
+        this._method = method;
+        this._message = message;
+    }
 };
 class UserNotFoundError extends Error{
+    string _module;
+    string _method;
+    string _message
+    public UserNotFoundError(string module, string method, string message){
+        this._module = module;
+        this._method = method;
+        this._message = message;
+    }
 
 };
 class IncorrectPasswordError extends Error{
-
+string _module;
+    string _method;
+    string _message
+    public IncorrectPasswordError(string module, string method, string message){
+        this._module = module;
+        this._method = method;
+        this._message = message;
+    }
 };
 
 module.exports = { InvalidInputError, DatabaseError, UserNotFoundError, IncorrectPasswordError};
