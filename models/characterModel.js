d65a2a49346a8ba9a2573ba5e0a896bca22330bd
SAnderson-96 <1360134@johnabbottcollege.net>
2022-04-28 13:58:50 -0400

    Changed some logs to use logger, and made some errors use the new way of doing it

--- models/characterModel.js
+++ models/characterModel.js
@@ -24,15 +24,15 @@ async function initialize(databaseNameTmp, reset) {
     if (reset) {
         //delete this query in the final version, this is here just for testing so you don't have to delete the entries after every time you run the code to debug.
         const deleteDbQuery = `Drop table if exists ${tableName};`;
-        await connection.execute(deleteDbQuery).then(logger.info(`Table: ${tableName} deleted if existed to reset the Db and reset increment in initialize()`)).catch((error) => { console.error(error); });
+        await connection.execute(deleteDbQuery).then(logger.info(`Table: ${tableName} deleted if existed to reset the Db and reset increment in initialize()`)).catch((error) => { throw new errors.DatabaseError('characterModel', 'intitalize', "Couldn't connect to the database"); });
     }
 
     const sqlQueryC = `Create table if not exists ${tableName}(id int AUTO_INCREMENT, name varchar(50), race VARCHAR(50), class VARCHAR(50), hitpoints int, primary key(id));`;
-    await connection.execute(sqlQueryC).then(logger.info(`Table: ${tableName} Created/Exists - initialize()`)).catch((error) => { console.error(error); });
+    await connection.execute(sqlQueryC).then(logger.info(`Table: ${tableName} Created/Exists - initialize()`)).catch((error) => { throw new errors.DatabaseError('characterModel', 'intitalize', "Couldn't connect to the database"); });
 }
 
 async function closeConnection() {
-    await connection.end().then(logger.info(`Connection closed from closeConnection() in dndModel`)).catch((error) => {console.log(error);});
+    await connection.end().then(logger.info(`Connection closed from closeConnection() in characterModel`)).catch((error) => { throw new errors.DatabaseError('characterModel', 'closeConnection', "Couldn't close the database connection"); });
 }
 
 //Region Add/Update/Delete Methods
@@ -48,13 +48,13 @@ async function closeConnection() {
  */
 async function addCharacter(name, race, charClass, hitpoints) {
     if (! await valUtils.isCharValid(name, race, charClass, hitpoints)) {
-        throw new errors.InvalidInputError("Invalid Character, cannot ADD in addCharacter()");
+        throw new errors.InvalidInputError('characterModel', 'addCharacter', "Invalid Character, cannot ADD");
     }
 
     //ADD CHAR TO DB
     let query = `insert into ${tableName} (name, race, class, hitpoints) values ('${name.toLowerCase()}', '${race.toLowerCase()}', '${charClass.toLowerCase()}', '${hitpoints}');`;
 
-    await connection.execute(query).then(console.log("Insert command executed in addCharacter()")).catch((error) => { throw new errors.DatabaseError(error); });
+    await connection.execute(query).then(logger.info("Insert command executed in addCharacter()")).catch((error) => { throw new errors.DatabaseError('characterModel', 'addCharacter', 'Couldn\'t execute the command'); });
 
 }
 
@@ -71,17 +71,17 @@ async function addCharacter(name, race, charClass, hitpoints) {
  */
 async function updateCharacter(id, newName, newRace, newClass, newHitpoints) {
     if (! await valUtils.isCharValid(newName, newRace, newClass, newHitpoints)) {
-        throw new errors.InvalidInputError("Invalid Character, cannot update character - updateCharacter()");
+        throw new errors.InvalidInputError('characterModel', 'updateCharacter', "Invalid Character, cannot update character");
     }
     let selectQuery = `Select 1 from ${tableName} where id = ${id}`;
-    let [rows, column_definitions] = await connection.query(selectQuery).then(console.log("select Query before Update Executed - updateCharacter()")).catch((error) => { throw new errors.DatabaseError(error); });
+    let [rows, column_definitions] = await connection.query(selectQuery).then(logger.info("select Query before Update Executed - updateCharacter()")).catch((error) => { throw new errors.DatabaseError('characterModel', 'updateCharacter', 'Couldn\'t execute the command'); });
 
     //Check if there is an ID that matches in the database
     if (rows.length == 0) {
-        throw new errors.InvalidInputError("Invalid Id, character DOES NOT EXIST!");
+        throw new errors.InvalidInputError('characterModel', 'updateCharacter', "Invalid Id, character DOES NOT EXIST!");
     }
     let query = `Update ${tableName} SET name = '${newName.toLowerCase()}', race = '${newRace.toLowerCase()}', class = '${newClass.toLowerCase()}', hitpoints = ${newHitpoints} where id = ${id};`;
-    await connection.execute(query).then(console.log("Update Query Executed - updateCharacter()")).catch((error) => { throw new errors.DatabaseError(error.message); });
+    await connection.execute(query).then(logger.info("Update Query Executed - updateCharacter()")).catch((error) => { throw new errors.DatabaseError(error.message); });
 }
 
 
@@ -137,7 +137,7 @@ async function findIdWithNameAndRace(name, race) {
  * @returns an object containing the character
  * @throws {InvalidInputError} If the character is not found 
  */
-async function getCharacter(id){
+async function getCharacter(id) {
     let query = `select id, name, race, class, hitpoints from ${tableName} where id = ${id};`;
     let [rows, column_definitions] = await connection.query(query).then(console.log("select Query before returning Character executed")).catch((error) => { throw new errors.DatabaseError(error); });
     if (rows.length === 0) {
@@ -153,23 +153,23 @@ async function getCharacter(id){
  * @throws {InvalidInputError} If the character is not found 
  * @throws {DatabaseError} If there was an error on the database's side
  */
-async function deleteCharacter(id){
+async function deleteCharacter(id) {
     let query = `delete from ${tableName} where id = ${id};`;
-    
-    try{
+
+    try {
         let checkingQ = `select * from ${tableName} where id = ${id};`;
         let [rows, column_definitions] = await connection.query(checkingQ).then(console.log("Select query to check if Id exists has been executed"));
 
-        if(rows.length === 0){
+        if (rows.length === 0) {
             throw new errors.InvalidInputError("Character not found with that ID");
         }
-        await connection.execute(query).then(console.log(`Delete Query Executed Character with id: ${id} was deleted`)).catch((error) => {throw new errors.DatabaseError("Delete Query could not be completed");});
+        await connection.execute(query).then(console.log(`Delete Query Executed Character with id: ${id} was deleted`)).catch((error) => { throw new errors.DatabaseError("Delete Query could not be completed"); });
         return true;
     }
-    catch (error){
+    catch (error) {
         throw error;
     }
-    
+
 }
 
 /**
@@ -188,18 +188,18 @@ async function printDb() {
  * Gets the connection to this database
  * @returns the connection to the database
  */
-function getConnection(){
+function getConnection() {
     return this.connection;
 }
 
-module.exports = { 
-    initialize, 
-    addCharacter, 
-    printDb, 
-    updateCharacter, 
-    findIdWithNameAndRace, 
-    closeConnection, 
-    hitpointsModifier, 
+module.exports = {
+    initialize,
+    addCharacter,
+    printDb,
+    updateCharacter,
+    findIdWithNameAndRace,
+    closeConnection,
+    hitpointsModifier,
     getCharacter,
     deleteCharacter,
     getConnection
