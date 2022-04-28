45c7107fb859a8131faeef247d472d32d97ea799
SAnderson-96 <1360134@johnabbottcollege.net>
2022-04-28 12:51:40 -0400

    added user controller

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const res = require('express/lib/response');
+const app = require('../app.js');
+const router = express.Router();
+const routeRootUsers = '/users';
+const routeRootSessions ='/sessions'
+model = require('../models/userModel');
+
+
+
