41b8e9c0b4f4ed69ae3f06e0ea93103d82d1e612
SAnderson-96 <1360134@johnabbottcollege.net>
2022-05-03 17:11:19 -0400

    Worked on the character sheet layout lol gg

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -81,9 +81,9 @@ async function updateHitpoints(request, response) {
  */
 async function getCharacter(request, response) {
     try {
-        let id = request.params.id;
-        let found = await model.getCharacter(id);
-        response.status(201).render('soloCharacter.hbs', { charactersActive: true, found: found });
+        var id = request.params.id;
+        // let found = await model.getCharacter(id);
+        response.status(201).render('characterSheet.hbs', { charactersActive: true, soloCharacter: 'soloCharacter.css'});
     }
     catch (error) {
         if (error instanceof errors.DatabaseError) {
@@ -93,6 +93,10 @@ async function getCharacter(request, response) {
         else if (error instanceof errors.InvalidInputError) {
             response.status(400).render('characters.hbs', { error: true, message: `Input error, Couldn't get Character with id: ${id}` });
             logger.error('input error - from getCharacter in characterController');
+        }
+        else{
+            response.status(400).render('characters.hbs', { error: true, message: `Catastrophic Failure` });
+            logger.error('Catastrophic Failure from getCharacter in characterController');
         }
     }
 }
@@ -123,6 +127,7 @@ async function getAllUserCharacters(request, response) {
         }
     }
 }
+
 
 
 /**
