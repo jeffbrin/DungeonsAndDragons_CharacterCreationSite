89f4b2cce040fc918c096f6ae44c5704dcd38187
Bringolf <2075606@PE322-19.local>
2022-05-06 12:09:27 -0400

    Fixed character stats model test

--- tests/models/characterStatisticsModel.test.js
+++ tests/models/characterStatisticsModel.test.js
@@ -1,43 +1,5 @@
-const characterStatsModel = require('../../models/characterStatisticsModel');
- const characterModel = require('../../models/characterModel');
- const spellModel = require('../../models/spellModel');
- const backgroundModel = require('../../models/backgroundModel');
- const classModel = require('../../models/classModel');
- const raceModel = require('../../models/raceModel');
- const fs = require('fs/promises');
- const dbName = 'dnd_db_testing';
 
- async function getAbilityNamesFromJsonFile(){
-    const abilities = JSON.parse(await fs.readFile('database-content-json/abilities.json'));
-    return abilities;
-}
-
-async function getSkillsFromJsonFile(){
-    const skills = JSON.parse(await fs.readFile('database-content-json/skills.json'));
-    return skills;
-}
-
- // Initialize the database before each test.
- beforeEach(async () => {
-     await classModel.initialize(dbName, true);
-     await backgroundModel.initialize(dbName, true);
-     await raceModel.initialize(dbName, true);
-     await spellModel.initialize(dbName, true);
-     await characterModel.initialize(dbName, true);
-     await characterStatsModel.initialize(dbName);   
-     await characterStatsModel.dropTables();
-});
-
- // Close the database connection after each test to prevent open handles error.
- afterEach(async () => {
-     await classModel.closeConnection();
-     await raceModel.closeConnection();
-     await backgroundModel.closeConnection();
-     await spellModel.closeConnection();
-     await characterStatsModel.closeConnection();
-     await characterModel.closeConnection();
- });
-
+// Get all skills
 test('getAllSkills - Success - Returns all skills in the json file in order.', async () => {
 
     const skills = await getSkillsFromJsonFile();
@@ -51,6 +13,14 @@ test('getAllSkills - Success - Returns all skills in the json file in order.', a
 
 })
 
+test('getAllSkill - Failure - Closed database connection.', async () => {
+
+    await characterStatsModel.closeConnection();
+    await expect(characterStatsModel.getAllSkills()).rejects.toThrow(DatabaseError);
+
+})
+
+// Get all abilities
 test('getAllAbilities - Success - Returns all abilities in the json file in order.', async () => {
 
     const abilities = await getAbilityNamesFromJsonFile();
@@ -62,3 +32,52 @@ test('getAllAbilities - Success - Returns all abilities in the json file in orde
     }
 
 })
+
+test('getAllAbilities - Failure - Closed database connection.', async () => {
+
+    await characterStatsModel.closeConnection();
+    await expect(characterStatsModel.getAllAbilities()).rejects.toThrow(DatabaseError);
+
+})
+
+
+// Set ability scores
+test('setAbilityScores - Success - Sets ability scores for character', async () => {
+
+    await characterModel.addCharacter(1, 1, 'Angongle', 10, 1, 1, 1, 1, [11, 12, 13, 14, 15, 16], [1, 3], 2, 1);
+    const character = await characterModel.getCharacter(1);
+
+
+
+})
+
+test('setAbilityScores - Failure - Closed database connection.', async () => {
+
+    await characterStatsModel.closeConnection();
+    await expect(characterStatsModel.getAllAbilities()).rejects.toThrow(DatabaseError);
+
+})
+
+// // Add skill proficiency
+//     addSkillProficiency,
+    
+// // Add skill expertise
+//     addSkillExpertise,
+    
+// // Add saving throw proficiency
+//     addSavingThrowProficiency,
+
+// // Set saving throw bonus
+//     setSavingThrowBonus,
+
+// // Get skill proficiencies
+//     getSkillProficiencies,
+
+// // Get saving throw bonuses
+//     getSavingThrowBonuses,
+
+// // Get saving throw proficiencies
+//     getSavingThrowProficiencies,
+
+// // Get skill expertise
+//     getSkillExpertise
