4b48f01279b2e82f9530a9c253d2dfc7ae642a3f
Jeffrey <bringolfj@hotmail.com>
2022-05-19 22:25:48 -0400

    Removed addCharacters from server.js.startup

--- server.js
+++ server.js
@@ -34,19 +34,6 @@ async function startup()
         await classModel.initialize(dbName, true);
         await spellModel.initialize(dbName, false);
         await characterModel.initialize(dbName, true);
-        await characterModel.addCharacter(3, 1, 'sam', 55, 2, 2, 2, 2, [1, 2, 3, 4, 5, 6], [1, 2], 3, 1, 25);
-        let character = await characterModel.getCharacter(1, 1);
-        console.log(character);
-        await characterModel.updateInitiative(1, 4);
-        await characterModel.updateSpeed(1, 30);
-        await characterModel.addItem(1, 'boots', 3);
-        await characterModel.addItem(1, 'boots', 4);
-        await characterModel.addCharacter(1, 2, "bob", 10, 3, 3, 1, 4, [16, 9, 5, 4, 8, 9], [2], 4, 1, 7);
-        let character2 = await characterModel.getCharacter(2, 1);
-        console.log(character2);
-        await characterModel.addCharacter(4, 5, "jeffrey", 10, 3, 3, 1, 4, [16, 9, 5, 4, 8, 9], [2], 4, 1, 7);
-        await characterModel.addCharacter(6, 3, "chase", 10, 3, 3, 1, 4, [16, 9, 5, 4, 8, 9], [2], 4, 1, 7);
-        await characterModel.addCharacter(1, 4, "Emperor Ligma", 10, 3, 3, 1, 4, [16, 9, 5, 4, 8, 9], [2], 4, 1, 7);
     } catch (error)
     {
         throw error;
